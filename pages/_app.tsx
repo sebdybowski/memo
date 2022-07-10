72387d2f3ce69324cd7dd5020288d93d5fee60bb
Seb Dybowski <dybowski.seb@gmail.com>
2022-07-10 19:25:17 +0200

    q30

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import 'milligram';
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { Fragment } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Fragment>
+    <Component {...pageProps} />
+    <br/>
+    <footer>by s3b1x</footer>
+  </Fragment>
 }
 
 export default MyApp
